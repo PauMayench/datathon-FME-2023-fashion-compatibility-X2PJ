e3bdc17bd32703e8aa5ded1472c7c9231a5b7c0b
PauMayench <paumayench@gmail.com>
2023-11-11 15:44:41 +0100

    .

---
+++ datathon/filterImages.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import preprocess_input
+import numpy as np
+
+def load_and_preprocess_image(img_path, target_size=(224, 224)): #returns the image of the path preprocessed, resized with target_size (the default is 224x224)
+
+
+    img = image.load_img(img_path, target_size=target_size)
+
+    img_array = image.img_to_array(img)
+
+    img_array = np.expand_dims(img_array, axis=0)
+
+    img_array = preprocess_input(img_array)
+
+    return img_array
+
+
+
+processed_image = load_and_preprocess_image('datathon/images/2019_43040692_OR.jpg' )
+print(processed_image)
