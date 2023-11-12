7f120941f6beb8276b29e97d799f0071ccab173a
PauMayench <paumayench@gmail.com>
2023-11-12 07:08:15 +0100

    add comments

--- datathon/NeuralNetworkTraining.py
+++ datathon/NeuralNetworkTraining.py
@@ -1,105 +1,86 @@
+# TensorFlow and other libraries import
 import tensorflow as tf
 import numpy as np
 import time as t
 import os
 import cv2
 
-
-'''
-this python code creates and trains a convolutional model, with the input data and stores the model on model_tensorflow
-
+''' This script creates and trains a convolutional neural network using TensorFlow. 
+ It processes image data from two categories, trains the model, and then saves it.
 '''
 
-categories = ['class1', 'class2'] # these are the two folders where we have the input of the model
+# Define the categories for classification
+categories = ['class1', 'class2'] # These represent the folders containing the dataset.
 
+# Set up path and data structures for storing images and labels
 PATH = os.getcwd()
+train_x = [] # List to store training images
+train_y = [] # List to store corresponding labels for training images
+test_x = [] # List to store testing images
+test_y = [] # List to store corresponding labels for testing images
 
-train_x = [] # arrays of images
-train_y = [] # array of the images' labels
+# Define the proportion of data to be used for testing
+percentage_testing = 0.05
 
-test_x = [] # arrays of images
-test_y = [] # array of the images' labels
-
-percentage_testing = 0.05 # percentage of the sample images that will be used only for testing
-
-max_samples_ini = 40000 #we put a bug number so it will do all of the tests, if you run with some problems like filling up the ram, you can pitch dowwn this value, it will also reduce the training data
+# Set a limit on the number of samples to be processed
+max_samples_ini = 40000
 max_samples = max_samples_ini
 
-# Save images from each category (class1 and class2)
+# Load and process images from each category
 for categ in categories:
     nou_path = os.path.join(PATH, categ)
     count = 0
     for path_imatge in os.listdir(nou_path):
-
         if (max_samples == 0):
             max_samples = max_samples_ini
             break
 
         max_samples -= 1
 
+        # Read and resize the image
         img = cv2.imread(os.path.join(PATH, categ, path_imatge))
-
-        resizeFact = 2.5    # we also can reduce or increment the size of the image, the higher this value, the more we reduce the sizze of each image
+        resizeFact = 2.5
         img = cv2.resize(img, (int(img.shape[1]/resizeFact), int(img.shape[0]/resizeFact)))
-       
-        #cv2.imshow("hehe", img)
-        #cv2.waitKey(0)
 
+        # Split data into training and testing sets
         count += 1
-
         if (count % (1/percentage_testing) == 0):
             count = 0
             test_x.append(img)
-            if (categ == categories[0]):
-                test_y.append(0)
-            else:
-                test_y.append(1)
-
+            test_y.append(0 if categ == categories[0] else 1)
         else:
             train_x.append(img)
-            if (categ == categories[0]):
-                train_y.append(0)
-            else:
-                train_y.append(1)
+            train_y.append(0 if categ == categories[0] else 1)
 
-
-print("TRAIN SAMPLES: ", len(train_x))
-print("TEST SAMPLES: ", len(test_x))
-
-
+# Convert lists to NumPy arrays for processing
 train_x = np.asarray(train_x)
 train_y = np.asarray(train_y)
-
 test_x = np.asarray(test_x)
 test_y = np.asarray(test_y)
 
-
-# INPUT: Matriu gran amb fotos de les prendes
-#print(train_x[0].shape)
-
-#The model declaration
+# Define the Convolutional Neural Network model
 model = tf.keras.models.Sequential([ 
-    tf.keras.layers.Conv2D(64, (5,5), activation = "relu", input_shape=(train_x[0].shape), padding='same'),
+    tf.keras.layers.Conv2D(64, (5,5), activation="relu", input_shape=(train_x[0].shape), padding='same'),
     tf.keras.layers.MaxPooling2D((3, 3)),
-    tf.keras.layers.Conv2D(32, (5,5), activation = "relu", padding='same'),
+    tf.keras.layers.Conv2D(32, (5,5), activation="relu", padding='same'),
     tf.keras.layers.MaxPooling2D((5, 5)),
     tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(32, activation = "relu"),
-    tf.keras.layers.Dense(32, activation = "relu"),
-    tf.keras.layers.Dense(2, activation = "softmax")
-    ])
+    tf.keras.layers.Dense(32, activation="relu"),
+    tf.keras.layers.Dense(32, activation="relu"),
+    tf.keras.layers.Dense(2, activation="softmax")
+])
 
+# Display model summary
 model.summary()
+t.sleep(3)
 
-t.sleep(3) # so we can read the output
-
+# Compile and train the model
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-
-
 model.fit(train_x, train_y, epochs=2, batch_size=128, verbose=1, shuffle=True)
 
-model.save('model_tensorflow') # The model is saved so that it can be loaded with the function: tf.keras.models.load_model('/tmp/model')
+# Save the trained model
+model.save('model_tensorflow')
 
+# Evaluate the model using the test dataset
 print("TRAINING FINISHED, STARTING TEST:")
-
 model.evaluate(test_x, test_y, verbose=2)
